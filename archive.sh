c7da8e1c8e01783fd138bd47a9340d744635874c
Nicholas Gower <nicholasgower01@gmail.com>
2025-08-14 12:02:11 -0700

    Update archive.sh

--- archive.sh
+++ archive.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=PlanetsLib_1.10.3/ -o PlanetsLib_1.10.3.zip HEAD
+git archive --prefix=PlanetsLib_1.10.6/ -o PlanetsLib_1.10.6.zip HEAD
