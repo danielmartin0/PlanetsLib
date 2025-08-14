fa61eb5d995bac4d3f04ff0b0cd062ff7aa09576
Nicholas Gower <nicholasgower01@gmail.com>
2025-08-14 12:12:08 -0700

    Raised version to 1.10.7

--- archive.sh
+++ archive.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=PlanetsLib_1.10.6/ -o PlanetsLib_1.10.6.zip HEAD
+git archive --prefix=PlanetsLib_1.10.7/ -o PlanetsLib_1.10.7.zip HEAD
