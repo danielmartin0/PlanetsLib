a58fd36d7b7e839fbcbd00ef3bb28b263a7fff48
Talandar <talandar66@protonmail.com>
2025-10-08 19:40:28 +0200

    commented out non needed blacklist

--- prototypes/mod-data.lua
+++ prototypes/mod-data.lua
@@ -20,12 +20,12 @@ local lock_silo_recipe_data = {
 
 
 data:extend{rocket_part_recipe_data,lock_silo_recipe_data}
-local blacklisted_planets = { --Planets with their own system for replacing rocket part recipes.
-    "muluna",
-    "maraxsis",
-}
-for _,planet in pairs(blacklisted_planets) do
-    PlanetsLib.assign_rocket_part_recipe(planet,"_other")
-end
+--local blacklisted_planets = { --Planets with their own system for replacing rocket part recipes.
+    --"muluna",
+    --"maraxsis",
+--}
+--for _,planet in pairs(blacklisted_planets) do
+    --PlanetsLib.assign_rocket_part_recipe(planet,"_other")
+--end
 -- For other mods, data.raw["mod-data"]["Planetslib-rocket-part-recipe"].data["muluna"] = "rocket-part-muluna"
 
