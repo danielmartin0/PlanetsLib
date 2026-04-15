93a5d9cdd222d43db9c4027825d60b63a530e8e9
Nicholas Gower <nicholasgower01@gmail.com>
2026-04-15 08:47:43 -0700

    Update archive script for version 1.17.2

--- archive.sh
+++ archive.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=PlanetsLib_1.14.2/ -o PlanetsLib_1.14.2.zip HEAD
+git archive --prefix=PlanetsLib_1.17.2/ -o PlanetsLib_1.17.2.zip HEAD
