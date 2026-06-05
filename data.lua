c8cf01f674aafa588483184ec7a1047072e76f99
Nicholas Gower <nicholasgower01@gmail.com>
2026-06-05 14:51:35 -0700

    Initial silo weight multiplier code

--- data.lua
+++ data.lua
@@ -3,6 +3,32 @@ require("prototypes.tiles")
 local orbits = require("lib.orbits")
 PlanetsLib = {}
 
+data:extend({
+	{
+		type = "mod-data",
+		name = "Planetslib",
+		data = {
+			unlinked_prerequisites = {},
+			planet_cargo_drop_whitelists = {
+				all = {
+					entity_types = {},
+					item_names = {},
+				},
+			},
+			rocket_silo_lift_multipliers = {
+				--[number(str)] == table {base_silo = replacement_silo}
+			},
+			planet_data = { --Place to store arbitrary planet properties to avoid a proliferation of new mod-data objects.
+				--[planet name] = table {incl. rocket_lift_multiplier}
+			}
+		},
+	},
+})
+
+PlanetsLib.constants = data.raw["mod-data"].Planetslib.data
+
+
+
 require("api")
 PlanetsLib.check_global_variables()
 require("prototypes.vanilla-override.recipe-productivity-technology")
@@ -17,18 +43,3 @@ if mods["space-age"] then
 	orbits.ensure_all_locations_have_orbits() -- We also call this in data-updates, but it's good to call it here as well so that if the vanilla planets are moved in data.lua (or before this code runs in data-updates.lua), we correctly recognize that their orbital children should be moved as well.
 end
 
-data:extend({
-	{
-		type = "mod-data",
-		name = "Planetslib",
-		data = {
-			unlinked_prerequisites = {},
-			planet_cargo_drop_whitelists = {
-				all = {
-					entity_types = {},
-					item_names = {},
-				},
-			},
-		},
-	},
-})
