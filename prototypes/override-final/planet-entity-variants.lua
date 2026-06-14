6aaf3afbbd3c801c9f87f73d9e1ca28b27e29e4c
Nicholas Gower <nicholasgower01@gmail.com>
2026-06-14 15:23:42 -0700

    Made loop that should only look for entity prototypes do so. https://github.com/danielmartin0/PlanetsLib/pull/98#discussion_r3400626054

--- prototypes/override-final/planet-entity-variants.lua
+++ prototypes/override-final/planet-entity-variants.lua
@@ -5,14 +5,18 @@ for _,planet in pairs(PlanetsLib.constants.on_entity_placed_on_planet_replacemen
         for _,item in pairs(data.raw["item"]) do
             if item.place_result == old_entity_name then
                 local new_entity
-                for _,prototype in pairs(data.raw) do
-                    for name,entity in pairs(prototype) do
-                        if name == new_entity_name then
-                            new_entity = entity
-                            break
+                for type, _ in pairs(defines.prototypes.entity) do
+                    for _,prototype in pairs(data.raw[type]) do
+                        for name,entity in pairs(prototype) do
+                            if name == new_entity_name then
+                                new_entity = entity
+                                break
+                            end
                         end
                     end
+
                 end
+                
                 new_entity.placeable_by = {item = item.place_result, count = 1}
             end
         end
