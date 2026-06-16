9012502d20933ab166c64842ba62f6ab66aea792
Nicholas Gower <nicholasgower01@gmail.com>
2026-06-15 17:57:22 -0700

    Rocket lift/part multiplier custom tooltips

--- prototypes/override-final/rocket-lift-multiplier.lua
+++ prototypes/override-final/rocket-lift-multiplier.lua
@@ -1,3 +1,4 @@
+local order = 101
 for _,planet in pairs(data.raw["planet"]) do
     if true or PlanetsLib.constants.planet_properties[planet.name] then
         local properties = PlanetsLib.constants.planet_properties[planet.name] or {}
@@ -5,6 +6,24 @@ for _,planet in pairs(data.raw["planet"]) do
         local part_multiplier =  properties["rocket_part_multiplier"]
         
         if lift_multiplier or part_multiplier then
+            if not planet.custom_tooltip_fields then planet.custom_tooltip_fields = {} end
+
+            if lift_multiplier then
+                table.insert(planet.custom_tooltip_fields,{
+                    name = {"surface-property-name.rocket-lift-multiplier"},
+                    value = {"surface-property-unit.multiplier",string.format(lift_multiplier,"%.2f")},
+                    order = order,
+                }   )  
+            end
+
+            if part_multiplier then
+                table.insert(planet.custom_tooltip_fields,{
+                    name = {"surface-property-name.rocket-part-multiplier"},
+                    value = {"surface-property-unit.multiplier",string.format(part_multiplier,"%.2f")},
+                    order = order,
+                }   )  
+            end
+
             --for _,silo in pairs(data.raw["rocket-silo"]) do
                 local silo = data.raw["rocket-silo"]["rocket-silo"]
                 local silo_item_name = silo.name
