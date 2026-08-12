b5c34683a275600402619a07599a9a3081166879
Nicholas Gower <nicholasgower01@gmail.com>
2026-08-12 14:22:37 -0700

    Changed baseURL of site

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,10 +11,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://danielmartin0.github.io',
+  url: 'https://planetslib.foundrygg.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'PlanetsLib',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
