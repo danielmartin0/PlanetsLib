47fd70450594a536dbd5d8b9f71f23b6ed6974cb
Nicholas Gower <nicholasgower01@gmail.com>
2026-08-12 16:39:14 -0700

    Finalize changelog for 1.24.0

--- archive.sh
+++ archive.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=PlanetsLib_1.23.5/ -o PlanetsLib_1.23.5.zip HEAD
+git archive --prefix=PlanetsLib_1.24.0/ -o PlanetsLib_1.24.0.zip HEAD
