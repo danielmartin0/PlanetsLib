438fdc5c6d244200d5a31548081a18da6a371b8a
Nicholas Gower <nicholasgower01@gmail.com>
2026-08-12 14:55:33 -0700

    Changed baseURL

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   url: 'https://planetslib.foundrygg.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/build/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
