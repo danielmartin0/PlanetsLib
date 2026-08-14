e37a0c6ede4693901be73186908e05f1cf295aad
Nicholas Gower <nicholasgower01@gmail.com>
2026-08-14 10:48:45 -0700

    Raised version to 1.25.1

--- archive.sh
+++ archive.sh
@@ -4,6 +4,6 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=PlanetsLib_1.25.0/ -o PlanetsLib_1.25.0.zip HEAD
+git archive --prefix=PlanetsLib_1.25.1/ -o PlanetsLib_1.25.1.zip HEAD
 
 sh update_documentation.sh
