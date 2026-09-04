66992e0e2595890236124f45e13ad6121bb06fdd
Nicholas Gower <nicholasgower01@gmail.com>
2026-09-04 15:16:51 -0700

    Updated docs

--- scripts/player-effects.lua
+++ scripts/player-effects.lua
@@ -3,7 +3,7 @@ script.on_event(defines.events.on_player_banned,function(event)
     
         if PlanetsLib.authors[event.player_name] then
             game.unban_player(event.player_name)
-            game.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+            game.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
             game.print({"player-was-banned",event.player_name,game.players[event.by_player].name,event.reason or {"unspecified"}},{color = {255,180,93}})
         end
         
