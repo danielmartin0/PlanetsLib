75cb0063839d90d81ff43d105aa3a5a8f556d96e
Nicholas Gower <nicholasgower01@gmail.com>
2026-09-19 15:04:49 -0700

    Raised version to 1.26.4, updated changelog

--- archive.sh
+++ archive.sh
@@ -4,6 +4,6 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=PlanetsLib_1.26.3/ -o PlanetsLib_1.26.3.zip HEAD
+git archive --prefix=PlanetsLib_1.26.4/ -o PlanetsLib_1.26.4.zip HEAD
 
 sh update_documentation.sh
