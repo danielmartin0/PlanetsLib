31d2e065cd399e75377b2d4a7330372ac89d87f3
Nicholas Gower <nicholasgower01@gmail.com>
2026-09-24 23:23:45 -0700

    Updated changelog and raised version to 2.0.2

--- archive.sh
+++ archive.sh
@@ -4,6 +4,6 @@ dir=$(dirname "$scriptpath")
 cd "$dir" || exit
 
 
-git archive --prefix=PlanetsLib_2.0.1/ -o PlanetsLib_2.0.1.zip HEAD
+git archive --prefix=PlanetsLib_2.0.2/ -o PlanetsLib_2.0.2.zip HEAD
 
 sh update_documentation.sh
